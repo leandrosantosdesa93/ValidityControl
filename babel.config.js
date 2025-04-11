8d871f780d33e8302786e45d70b2245ae395f746
Leandro de Sa <leandro.santosdesa@outlook.com>
2025-04-11 15:05:12 -0300

    correção de erros

--- babel.config.js
+++ babel.config.js
@@ -23,7 +23,9 @@ module.exports = function (api) {
       'react-native-reanimated/plugin',
       ['@babel/plugin-transform-class-properties', { loose: true }],
       ['@babel/plugin-transform-private-methods', { loose: true }],
-      ['@babel/plugin-transform-private-property-in-object', { loose: true }]
+      ['@babel/plugin-transform-private-property-in-object', { loose: true }],
+      '@babel/plugin-transform-nullish-coalescing-operator',
+      '@babel/plugin-transform-optional-chaining'
     ],
     env: {
       production: {
--- metro.config.js
+++ metro.config.js
@@ -1,37 +1,32 @@
-const {getDefaultConfig} = require('@react-native/metro-config');
+const { getDefaultConfig } = require('expo/metro-config');
 const path = require('path');
 
-module.exports = (async () => {
-  const defaultConfig = await getDefaultConfig(__dirname);
-  const { assetExts, sourceExts } = defaultConfig.resolver;
+const config = getDefaultConfig(__dirname);
 
-  return {
-    ...defaultConfig,
-    transformer: {
-      ...defaultConfig.transformer,
-      unstable_allowRequireContext: true,
-      babelTransformerPath: require.resolve('react-native-svg-transformer'),
-      getTransformOptions: async () => ({
-        transform: {
-          experimentalImportSupport: false,
-          inlineRequires: true,
-        },
-      }),
-    },
-    resolver: {
-      ...defaultConfig.resolver,
-      assetExts: assetExts.filter((ext) => ext !== 'svg'),
-      sourceExts: [...sourceExts, 'svg', 'jsx', 'js', 'ts', 'tsx', 'json'],
-      extraNodeModules: {
-        '@': path.resolve(__dirname, 'src'),
-        '@components': path.resolve(__dirname, 'components'),
-        '@constants': path.resolve(__dirname, 'constants'),
-        '@hooks': path.resolve(__dirname, 'hooks'),
-        '@services': path.resolve(__dirname, 'src/services'),
-        '@store': path.resolve(__dirname, 'src/store'),
-        '@types': path.resolve(__dirname, 'src/types'),
-        '@utils': path.resolve(__dirname, 'src/utils')
-      }
-    }
-  };
-})(); 
+// Adicione suporte para SVG
+const { transformer, resolver } = config;
+config.transformer = {
+  ...transformer,
+  babelTransformerPath: require.resolve('react-native-svg-transformer'),
+  unstable_allowRequireContext: true,
+  minifierPath: 'metro-minify-terser',
+  minifierConfig: {}
+};
+
+config.resolver = {
+  ...resolver,
+  assetExts: resolver.assetExts.filter((ext) => ext !== 'svg'),
+  sourceExts: [...resolver.sourceExts, 'svg', 'jsx', 'js', 'ts', 'tsx', 'json'],
+  extraNodeModules: {
+    '@': path.resolve(__dirname, 'src'),
+    '@components': path.resolve(__dirname, 'components'),
+    '@constants': path.resolve(__dirname, 'constants'),
+    '@hooks': path.resolve(__dirname, 'hooks'),
+    '@services': path.resolve(__dirname, 'src/services'),
+    '@store': path.resolve(__dirname, 'src/store'),
+    '@types': path.resolve(__dirname, 'src/types'),
+    '@utils': path.resolve(__dirname, 'src/utils')
+  }
+};
+
+module.exports = config; 
